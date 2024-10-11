b59672e447e0c5a7c9ffe29891ac4c0a74f42f76
João Vitor Justino de Lima <joaovitorjustinodelima@gmail.com>
2024-10-11 01:07:23 -0300

    Continunado com os teste de monitor no versel

--- src/components/item-box.jsx
+++ src/components/item-box.jsx
@@ -6,7 +6,7 @@ export default function ItemBox() {
             <div>
                 <img src={camiseta} />
                 <h3 className="text-1 font-bold pt-4 text-left mb-2">Camiseta Basica</h3>
-                <h3 className="text-2 font-bold text-left">R$ 79,00</h3>
+                <h3 className="text-2 font-bold text-left pb-4">R$ 79,00</h3>
             </div>
         </button>
 
--- src/pages/shopping-page.jsx
+++ src/pages/shopping-page.jsx
@@ -2,17 +2,56 @@ import ItemBox from "../components/item-box";
 
 export default function ShoppingPage() {
     return (
-        <div className="flex flex-row">
-            <div className="basis-1/3 pl-[10%] md:pl-20 sm:pl-10 md:basis-1/4 sm:basis-1/5">
-                <div className="flex flex-col  w-80 px-10 pt-5 border-solid border-2 border-slate-200 rounded-lg sm:w-auto">
-                    <div className="h-60">
+        <div className="flex flex-row px-[10%] gap-4">
+            <div className="flex flex-initial basis-1/3 md:basis-1/4 sm:basis-1/5">
+                <div className="flex flex-col w-auto h-max px-10 pt-5 border-solid border-2 border-slate-200 rounded-lg">
+                    <div className="h-max">
                         <h1 className="text-3xl font-bold md:text-xl sm:text-lg">Filtro</h1>
-                        <div>
-                            tipo de roupa
+                        <div className="flex flex-wrap justify-start gap-1 pb-4">
+                            <div>
+                                <input type="checkbox" id="camiseta" value="" className="hidden peer" required="" />
+                                <label for="camiseta" className="inline-flex items-center justify-center p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">Camisetas</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="camiseta" value="" className="hidden peer" required="" />
+                                <label for="camiseta" className="inline-flex items-center justify-center p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">Camisas</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="camiseta" value="" className="hidden peer" required="" />
+                                <label for="camiseta" className="inline-flex items-center justify-center p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">Blusas</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="camiseta" value="" className="hidden peer" required="" />
+                                <label for="camiseta" className="inline-flex items-center justify-center p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">Calças</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="camiseta" value="" className="hidden peer" required="" />
+                                <label for="camiseta" className="inline-flex items-center justify-center p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">Assesorios</div>
+                                    </div>
+                                </label>
+                            </div>
                         </div>
                         <hr className="w-[90%] h-px mx-auto bg-slate-200 border-0" />
                     </div>
-                    <div className="h-32">
+                    <div className="h-max">
                         <h1 className="text-3xl font-bold md:text-xl sm:text-lg">Preço</h1>
                         <div>
                             <div className="price-range p-4">
@@ -27,18 +66,79 @@ export default function ShoppingPage() {
                         </div>
                         <hr className="w-[90%] h-px mx-auto bg-slate-200 border-0" />
                     </div>
-                    <div className="h-64">
+                    <div className="h-max">
                         <h1 className="text-3xl font-bold md:text-xl sm:text-lg">Tamanho</h1>
-                        <div>
-                            botoes de tamanhos
+                        <div className="flex flex-wrap justify-start gap-1 pb-4">
+                            <div>
+                                <input type="checkbox" id="tamanhoP" value="" className="hidden peer" required="" />
+                                <label for="tamanhoP" className="inline-flex items-center justify-center w-11 p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">P</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="tamanhoM" value="" className="hidden peer" required="" />
+                                <label for="tamanhoM" className="inline-flex items-center justify-center w-11 p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">M</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="tamanhoG" value="" className="hidden peer" required="" />
+                                <label for="tamanhoG" className="inline-flex items-center justify-center w-11 p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">G</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="tamanhoGG" value="" className="hidden peer" required="" />
+                                <label for="tamanhoGG" className="inline-flex items-center justify-center w-11 p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">GG</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="tamanhoG1" value="" className="hidden peer" required="" />
+                                <label for="tamanhoG1" className="inline-flex items-center justify-center w-11 p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">G1</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="tamanhoG2" value="" className="hidden peer" required="" />
+                                <label for="tamanhoG2" className="inline-flex items-center justify-center w-11 p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">G2</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="tamanhoG3" value="" className="hidden peer" required="" />
+                                <label for="tamanhoG3" className="inline-flex items-center justify-center w-11 p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">G3</div>
+                                    </div>
+                                </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="tamanhoG4" value="" className="hidden peer" required="" />
+                                <label for="tamanhoG4" className="inline-flex items-center justify-center w-11 p-2 font-medium tracking-tight border rounded-lg cursor-pointer bg-brand-light text-brand-black border-violet-500 peer-checked:border-violet-400 peer-checked:bg-violet-600 peer-checked:text-white peer-checked:font-semibold peer-checked:decoration-brand-dark decoration-2">
+                                    <div className="block">
+                                        <div className="w-full text-lg font-semibold">G4</div>
+                                    </div>
+                                </label>
+                            </div>
                         </div>
-                        <hr className="w-[90%] h-px mx-auto bg-slate-200 border-0" />
                     </div>
                 </div>
             </div>
             <div>
-                <h1>Camisetas</h1>
-                <div className="grid gap-8 sm:gap-2 grid-cols-3 p-3 mb-10 pr-[10%] md:pr-20 sm:pr-10">
+                <div className="grid gap-4 grid-cols-3 sm:grid-cols-2  p-3 mb-10">
                     <ItemBox />
                     <ItemBox />
                     <ItemBox />
